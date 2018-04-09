d61b8d10e97c8cecef85e41a33c73107e8ab0aff
Venkat <vravuri@vmware.com>
2018-04-09 10:10:51 +0100

    introduced more aggregated features

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -3,35 +3,107 @@ import os
 import logging
 import pandas as pd
 import numpy as np
+from datetime import datetime
+import gc
+
+
+# Credits: https://www.kaggle.com/nanomathias/feature-engineering-importance-testing
+def generateAggregateFeatures(train_df, aggregateFeatures):
+    logger = logging.getLogger("generateAggregateFeatures")
+    for spec in aggregateFeatures:
+        logger.info("Generating aggregate feature {} group by {}, \
+         and aggregating {} with {}".format(spec['name'], \
+                                            spec['groupBy'], spec['select'], spec['agg']))
+        gp = train_df[spec['groupBy'] + [spec['select']]] \
+            .groupby(by=spec['groupBy'])[spec['select']] \
+            .agg(spec['agg']) \
+            .reset_index() \
+            .rename(index=str, columns={spec['select']: spec['name']})
+        train_df = train_df.merge(gp, on=spec['groupBy'], how='left')
+        del gp
+        gc.collect()
+
+    return train_df
+
+
+def generateNextClickFeatures(train_df, nextClickAggregateFeatures):
+    logger = logging.getLogger("generateNextClickFeatures")
+    for spec in nextClickAggregateFeatures:
+        feature_name = '{}-next-click'.format('-'.join(spec['groupBy']))
+        logger.info("Generating feature '{0}'".format(feature_name))
+        train_df[feature_name] = train_df[spec['groupBy'] + ['click_time']].groupby(['ip']).click_time.transform(
+            lambda x: x.diff().shift(-1)).dt.seconds
+    return train_df
+
+
+def extractTimeInformation(X_train_df):
+    X_train_df['day'] = X_train_df.click_time.dt.day.astype('uint8')
+    X_train_df['hour'] = X_train_df.click_time.dt.hour.astype('uint8')
+    X_train_df['minute'] = X_train_df.click_time.dt.minute.astype('uint8')
+    X_train_df['second'] = X_train_df.click_time.dt.second.astype('uint8')
+
 
 def main():
+    nrows = None
     logger = logging.getLogger(__name__)
 
     dtypes = {'ip': np.uint32, 'app': np.uint16, 'device': np.uint8, 'os': np.uint8, 'channel': np.uint8,
               'is_attributed': np.uint8}
-    X_train_df = pd.read_csv(project_dir + '/data/raw/train.csv', sep=',', dtype=dtypes,
+    X_train_df = pd.read_csv(project_dir + '/data/raw/train.csv', sep=',', nrows=nrows, dtype=dtypes,
                              parse_dates=['click_time', 'attributed_time'])
-
+    logger.info("Total records: {0}".format(str(X_train_df.size)))
     logger.info("Unique 'ip' count: {0}".format(str(X_train_df.ip.unique().size)))
     logger.info("Unique 'app' count: {0}".format(str(X_train_df.app.unique().size)))
-    logger.info("Unique 'device' & 'os' combinations count: {0}".format(str(X_train_df.groupby(['device', 'os']).size().size)))
     logger.info("Unique 'channel' count: {0}".format(str(X_train_df.channel.unique().size)))
-    logger.info("Total downloads count in training set: {0}".format(str(X_train_df[X_train_df.is_attributed == 1].size)))
+    logger.info("Total downloads count in training set: {0}" \
+                .format(str(X_train_df[X_train_df.is_attributed == 1].size)))
 
-    X_train_df.drop(['attributed_time'], axis=1, inplace=True)
+    extractTimeInformation(X_train_df)
 
-    X_train_df['device_os'] = X_train_df['device'] * 1000 + X_train_df['os']
-    X_train_df.drop(['device'], axis=1, inplace=True)
-    X_train_df.drop(['os'], axis=1, inplace=True)
+    aggregateFeatures = [
+        # Number of clickes for ip-app
+        {'name': 'ip-app-count', 'groupBy': ['ip', 'app'], 'select': 'channel', 'agg': 'count'},
+        # Number of clicks for each ip-app-os
+        {'name': 'ip-app-os-count', 'groupBy': ['ip', 'app', 'os'], 'select': 'channel', 'agg': 'count'},
+        # Number of clicks for ip-day-hour
+        {'name': 'ip-day-hour-count', 'groupBy': ['ip', 'day', 'hour'], 'select': 'channel', 'agg': 'count'},
+        # Number of clicks for ip-app-day-hour
+        {'name': 'ip-app-day-hour-count', 'groupBy': ['ip', 'app', 'day', 'hour'], 'select': 'channel', 'agg': 'count'},
+        # Clicks variance in day, for ip-app-channel
+        {'name': 'ip-app-channel-var', 'groupBy': ['ip', 'app', 'channel'], 'select': 'day', 'agg': 'var'},
+        # Clicks variance in hour, for ip-app-os
+        {'name': 'ip-app-os-var', 'groupBy': ['ip', 'app', 'os'], 'select': 'hour', 'agg': 'var'},
+        # Clicks variance in hour, for ip-day-channel
+        {'name': 'ip-day-channel-var', 'groupBy': ['ip', 'day', 'channel'], 'select': 'hour', 'agg': 'var'},
+        # Mean clicks in an hour, for ip-app-channel
+        {'name': 'ip-app-channel-mean', 'groupBy': ['ip', 'app', 'channel'], 'select': 'hour', 'agg': 'mean'},
+        # How popular is the app in channel?
+        {'name': 'app-popularity', 'groupBy': ['app'], 'select': 'channel', 'agg': 'count'},
+        # How popular is the channel in app?
+        {'name': 'channel-popularity', 'groupBy': ['channel'], 'select': 'app', 'agg': 'count'},
+        # Average clicks on app by distinct users; is it an app they return to?
+        {'name': 'avg-clicks-on-app', 'groupBy': ['app'], 'select': 'ip',
+         'agg': lambda x: float(len(x)) / len(x.unique())}
+    ]
+    X_train_df = generateAggregateFeatures(X_train_df, aggregateFeatures)
 
-    X_train_df['click_dayofyear'] = X_train_df['click_time'].dt.dayofyear
-    X_train_df['click_hour'] = X_train_df['click_time'].dt.hour
-    X_train_df['click_minute'] = X_train_df['click_time'].dt.minute
-    X_train_df.drop(['click_time'], axis=1, inplace=True)
+    nextClickAggregateFeatures = [
+        {'groupBy': ['ip']},
+        {'groupBy': ['ip', 'app']},
+        {'groupBy': ['ip', 'channel']},
+        {'groupBy': ['ip', 'os']}
+    ]
+    X_train_df = generateNextClickFeatures(X_train_df, nextClickAggregateFeatures)
 
-    columns = ['ip', 'app', 'device_os', 'channel', 'click_dayofyear', 'click_hour', 'click_minute', 'is_attributed']
+    columns = ['ip', 'app', 'device', 'os', 'channel', 'click_time', 'attributed_time', \
+               'is_attributed', 'day', 'hour', 'minute', 'second', \
+               'ip-app-count', 'ip-app-os-count', 'ip-day-hour-count', 'ip-app-day-hour-count', 'ip-app-channel-var', \
+               'ip-app-os-var', 'ip-day-channel-var', 'ip-app-channel-mean', 'app-popularity', 'channel-popularity',
+               'avg-clicks-on-app', 'ip-next-click', 'ip-app-next-click', 'ip-channel-next-click', 'ip-os-next-click']
 
-    X_train_df.to_csv(project_dir + '/data/processed/train.csv', sep=',', columns=columns, index=False)
+    X_train_df.to_csv(project_dir + '/data/processed/train' + datetime.now().strftime("-%Y-%m-%d-%H-%M-%S") + '.csv',
+                      sep=',', columns=columns, index=False)
+
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
